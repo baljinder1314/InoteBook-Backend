403d07828c9cac0a797708e309ccae92e1008f2a
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-14 16:18:54 +0530

    Fix CORS configuration: remove specific origin to allow all origins

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(
   cors({
-    origin: ["*", "inote-book-backend-one.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
