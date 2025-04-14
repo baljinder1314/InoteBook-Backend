ba8ac26b86a74d119c772958b9a1d09de4ad21b0
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-14 16:23:21 +0530

    Fix CORS configuration: simplify origin setting to allow all origins

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(
   cors({
-    origin: ["*"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
