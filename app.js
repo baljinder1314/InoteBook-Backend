896f630448e7dd77214689248784528a5cdded39
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-14 23:29:21 +0530

    Fix CORS configuration: allow specific origin alongside wildcard

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(
   cors({
-    origin: "*",
+    origin: ["*", "https://incredible-caramel-eba54d.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
