d0c699083b85fae588790c1dee3734f1b072fc3b
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-15 22:11:33 +0530

    Fix CORS configuration: update allowed origin to match frontend deployment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://inotebook-backend-wz64.onrender.com"],
+    origin: ["http://localhost:5173", "https://inotebook-front-xi.vercel.app"],
      
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
