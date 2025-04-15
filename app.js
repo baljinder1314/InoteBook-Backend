774ab291cfa936c1301e87e8518f9168847ac445
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-15 21:24:36 +0530

    Fix CORS configuration: update allowed origin to match backend deployment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(
   cors({
-    origin: ["*", "https://incredible-caramel-eba54d.netlify.app/"],
+    origin: ["*", "https://inotebook-backend-wz64.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
